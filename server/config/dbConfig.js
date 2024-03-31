7da0d8ed2adcf23ca500eaef5464e24c4377ab4b
thanitsakdue <93909305+thanitsakdue@users.noreply.github.com>
2024-03-31 18:44:08 +0700

    Myproject
    1st Commit

--- server/config/dbConfig.js
+++ server/config/dbConfig.js
@@ -1,24 +1,26 @@
-const mongoose = require('mongoose');
+    require('dotenv').config();
+    const mongoose = require('mongoose');
 
-/**
- * ตั้งค่าการใช้งานคิวรี่โดยเคร่งความเข้มงวดเป็นเท็จใน Mongoose.
- */
-mongoose.set('strictQuery', false)
+    /**
+     * ตั้งค่าการใช้งานคิวรี่โดยเคร่งความเข้มงวดเป็นเท็จใน Mongoose.
+     */
+    mongoose.set('strictQuery', false)
 
-/**
- * เชื่อมต่อกับฐานข้อมูล MongoDB โดยใช้ URL ที่กำหนดในตัวแปรสิ่งแวดล้อม mongo_url.
- * @param {string} process.env.mongo_url URL ของ MongoDB
- * @return {mongoose.Connection} การเชื่อมต่อกับฐานข้อมูล MongoDB
- */
-mongoose.connect(process.env.mongo_url)
+    /**     
+     * เชื่อมต่อกับฐานข้อมูล MongoDB โดยใช้ URL ที่กำหนดในตัวแปรสิ่งแวดล้อม mongo_url.
+     * @param {string} process.env.mongo_url URL ของ MongoDB
+     * @return {mongoose.Connection} การเชื่อมต่อกับฐานข้อมูล MongoDB
+     */
+    mongoose.connect(process.env.mongo_url)
 
-const connection = mongoose.connection;
+    const connection = mongoose.connection;
 
-connection.on('connected' , ()=>{
-    console.log('Mongo DB Connected');
-})
+    connection.on('connected' , ()=>{
+        console.log('Mongo DB Connected');
+    })
 
-connection.on('error' , (err)=>{
-    console.log('Mongo DB Failed');
-})
+    connection.on('error' , (err)=>{
+        console.log('Mongo DB Failed');
+    })
+    
 
